6e04aabd97b08d6dc574f4a3e9d3d956c2155a98
tanzhixuan <tanzhixuan@bytedance.com>
2021-03-08 16:42:44 +0800

    fix: 增加自定义字符

--- web-tools/src/pages/string-tools/useSpecialChar.tsx
+++ web-tools/src/pages/string-tools/useSpecialChar.tsx
@@ -1,11 +1,21 @@
-import { useRef } from 'react';
+import { Button, message, Modal, Select } from 'antd';
+import { useMemo, useRef, useState } from 'react';
 import { UseAction } from './interface';
 import { SPECIAL_CHAR } from './mockData';
 
 function getColumnData(str: string) {
     const words = str.split('\n');
 
-    const specialChar = new RegExp("[`~!@#$^&*()=|{}':;',\\[\\].<>《》/?%~！@#￥……&*（）——|{}【】‘；：”“'。，、？]");
+    let customChars = []
+    const storage = localStorage[SPECIAL_CUSTOM_KEY]
+    if (storage) {
+        try {
+            customChars = JSON.parse(storage)
+        } catch(e) {
+            customChars = []
+        }
+    }
+    const specialChar = new RegExp("[`~!@#$^&*()=|{}':;',\\[\\].<>《》/?%~！@#￥……&*（）——|{}【】‘；：”“'。，、？+′" + customChars.map((i: string) => `\\${i}`).join('') + ']');
     const canU: (string | number)[] = [];
     words.forEach(i => {
         
@@ -58,16 +68,80 @@ const useSpecialChar: () => UseAction = function() {
         v.current = getColumnData(s)
         return v.current
     }
-
+    const [showModal, setShowModal] = useState(false)
     const getStringValue = () => v.current.map(i => `${i.o}\t${i.t}`).join('\n')
-
+    const renderOtherTool = () => <>
+        <Button type="text" onClick={() => setShowModal(true)}>增加自定义特殊符号</Button>
+        <FillModal showModal={showModal} setShowModal={setShowModal} />
+    </>
     return {
         renderResult,
         compute,
         showCopyButton: true,
         getStringValue,
         getMockData: () => SPECIAL_CHAR,
+        renderOtherTool,
     }
 }
+interface FillModalProps {
+    showModal: boolean
+    setShowModal: (_b: boolean) => void
+}
+
+const SPECIAL_CUSTOM_KEY = 'special_custom'
+
+function FillModal(props: FillModalProps) {
+    const ref = useRef<string[]>([])
+
+    function handleChange(value: any) {
+        console.log(`Selected: ${typeof value}`)
+        ref.current = value
+    }
+
+    const defaultV = useMemo<string[]>(() => {
+        let o = localStorage[SPECIAL_CUSTOM_KEY];
+        if (o) {
+            try {
+                o = JSON.parse(o)
+            } catch(e) {
+                o = []
+            }
+        } else {
+            o = []
+        }
+        return o
+    }, []);
+
+    function handleOk() {
+        const newStr = ref.current;
+        
+        if (newStr) {
+            const arr = newStr.filter(i => i.length === 1)
+            localStorage[SPECIAL_CUSTOM_KEY] = JSON.stringify(arr)
+            message.success('成功写入！')
+        }
+        props.setShowModal(false)
+    }
+
+    return <Modal
+        title="录入自定义特殊字符"
+        centered
+        visible={props.showModal}
+        onOk={handleOk}
+        onCancel={() => props.setShowModal(false)}
+        width={800}
+    >
+        <Select
+            mode="tags"
+            size="middle"
+            placeholder="输入需要添加的自定义字符，输入一个字符后按回车增加，最后点击 ok"
+            defaultValue={defaultV}
+            onChange={handleChange}
+            style={{ width: '100%' }}
+        >
+        </Select>
+    </Modal>
+}
+
 
 export default useSpecialChar;
