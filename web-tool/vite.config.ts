ef73432ba299c0a8b2386edb330685d15d667f6c
tanzhixuan <tanzhixuan@senseauto.com>
2023-04-23 11:24:32 +0800

    fix

--- web-tool/vite.config.ts
+++ web-tool/vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: path.resolve('..', 'public', 'web-tools'),
+  },
   plugins: [
     react(),
     VitePWA(),
